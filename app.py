f25607a9f0b37acda0511ba05da78c50dc1cf193
Garima <114860666+Garima-Bazard@users.noreply.github.com>
2022-12-20 23:53:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from pandas.core.generic import pickle
+from sklearn.ensemble import RandomForestRegressor
+
+header = st.container()
+features = st.container()
+
+with header:
+    st.title("Delhi Flats Price!")
+    st.text("In this project the price of Flats in Delhi are predicted...")
+
+
+     
+
 
