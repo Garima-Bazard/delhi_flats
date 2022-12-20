caa68f6a9e375e0449fba67fc483df6390526cdf
Garima <114860666+Garima-Bazard@users.noreply.github.com>
2022-12-21 00:06:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from pandas.core.generic import pickle
 #from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 header = st.container()
 features = st.container()
@@ -10,7 +11,14 @@ features = st.container()
 with header:
     st.title("Delhi Flats Price!")
     st.text("In this project the price of Flats in Delhi are predicted...")
-
+    
+    
+    
+with features:
+    st.header("Inputs")
+    st.text("Please enter the relevant data for price prediction")
+    
+     balconies_sel = sel_col.slider("Balcony",min_value=1,max_value=10,value=0,step=1)
 
      
 
