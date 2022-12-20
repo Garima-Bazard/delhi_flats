ba508458bf4480d82b6d5f50a845204f0453218e
Garima <114860666+Garima-Bazard@users.noreply.github.com>
2022-12-21 00:12:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from pandas.core.generic import pickle
 #from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestRegressor
